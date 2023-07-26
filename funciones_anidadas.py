2c7bf78b3a2aee5e6705fb5893d79f49eafcdd71
= <=>
2023-07-26 11:44:50 -0500

    version cuatro

--- funciones_anidadas.py
+++ funciones_anidadas.py
@@ -4,7 +4,11 @@ def primera(datos):
     def segunda():
         for n in datos:
             print(n)
-    
+        
+        def tercera():
+            print("Hola desde la tercera funcion")
+        tercera()
+        
     segunda()
             
 primera([2, 4, 6, 8, 10])
